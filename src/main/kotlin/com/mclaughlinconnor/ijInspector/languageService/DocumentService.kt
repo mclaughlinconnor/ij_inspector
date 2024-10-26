f4d73f8e176af8ca053ddab1e8874228a7a9099a
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-10-26 22:49:30 +0100

    perf: improve performance of diagnostics by setting the selected editor at better times

--- src/main/kotlin/com/mclaughlinconnor/ijInspector/languageService/DocumentService.kt
+++ src/main/kotlin/com/mclaughlinconnor/ijInspector/languageService/DocumentService.kt
@@ -5,11 +5,10 @@ import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
 import com.intellij.openapi.application.Application
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiFile
 import com.mclaughlinconnor.ijInspector.lsp.DidChangeTextDocumentParams
 import com.mclaughlinconnor.ijInspector.utils.Utils
 
@@ -30,29 +29,24 @@ class DocumentService(
     fun handleChange(filePath: String, params: DidChangeTextDocumentParams) {
         val document = Utils.createDocument(myProject, filePath) ?: return
 
-        var file: PsiFile? = null
-        myApplication.runReadAction {
-            file = PsiDocumentManager.getInstance(myProject).getPsiFile(document)
-        }
-        if (file == null) {
-            return
-        }
-
         myApplication.invokeLater {
+            val file = PsiDocumentManager.getInstance(myProject).getPsiFile(document) ?: return@invokeLater
+            fileEditorManager.openFile(file.virtualFile, true)
+            fileEditorManager.setSelectedEditor(file.virtualFile, TextEditorProvider.getInstance().editorTypeId)
+
             WriteCommandAction.runWriteCommandAction(myProject) {
                 document.setText(params.contentChanges[0].text) // only support whole document updates
                 psiDocumentManager.commitDocument(document)
             }
-            fileEditorManager.openFile(file!!.virtualFile, true)
         }
     }
 
     fun doOpen(filePath: String) {
         val document = Utils.createDocument(myProject, filePath) ?: return
         myApplication.invokeLater {
-            EditorFactory.getInstance().createEditor(document, myProject) ?: return@invokeLater
             val file = PsiDocumentManager.getInstance(myProject).getPsiFile(document) ?: return@invokeLater
             fileEditorManager.openFile(file.virtualFile, true)
+            fileEditorManager.setSelectedEditor(file.virtualFile, TextEditorProvider.getInstance().editorTypeId)
         }
     }
 }
