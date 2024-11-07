2165898daeb3d8abb72ed990eb6974b2bfcfa34f
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-11-07 23:24:56 +0000

    fix: Fix serialisation of `CompletionList.isIncomplete`

--- src/main/kotlin/com/mclaughlinconnor/ijInspector/lsp/CompletionList.kt
+++ src/main/kotlin/com/mclaughlinconnor/ijInspector/lsp/CompletionList.kt
@@ -3,20 +3,11 @@ package com.mclaughlinconnor.ijInspector.lsp
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class CompletionList(
-    /**
-     * This list is not complete. Further typing should result in recomputing
-     * this list.
-     *
-     * Recomputed lists have all their items replaced (not appended) in the
-     * incomplete completion sessions.
-     */
-    @JsonProperty
+    @get:JsonProperty("isIncomplete")
     val isIncomplete: Boolean,
-    /**
-     * The completion items.
-     */
+
     @JsonProperty
-    val items: MutableList<CompletionItem>
+    val items: MutableList<CompletionItem>,
 ) {
     fun pushItem(item: CompletionItem) {
         items.add(item)
