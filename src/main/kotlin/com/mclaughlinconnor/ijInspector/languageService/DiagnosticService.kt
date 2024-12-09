fb6ff3cb042fb94fac1e5001bded356261a3012a
Connor McLaughlin <connor1211.mcl@gmail.com>
2024-12-09 23:09:08 +0000

    chore: further tweak diagnostics timeouts

--- src/main/kotlin/com/mclaughlinconnor/ijInspector/languageService/DiagnosticService.kt
+++ src/main/kotlin/com/mclaughlinconnor/ijInspector/languageService/DiagnosticService.kt
@@ -44,7 +44,7 @@ class DiagnosticService(
         startListening()
     }
 
-    fun triggerDiagnostics(files: List<PsiFile>, timeoutMillis: Long = 500) {
+    fun triggerDiagnostics(files: List<PsiFile>, timeoutMillis: Long = 200) {
         println("Triggering diagnostics for files: ${files.map { it.virtualFile.path }}")
         ProgressManager.getInstance().run(object : Task.Backgroundable(myProject, "Running diagnostics...", false) {
             override fun run(indicator: ProgressIndicator) {
